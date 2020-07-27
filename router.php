8135d6454a6ea86e65140ef89b1e317261c1376a
Rafa-upsrj <67658320+Rafa-upsrj@users.noreply.github.com>
2020-07-27 12:01:55 -0500

    actualizacion router.php
    se actualizo para que funcionara en sistema operativo windows

--- router.php
+++ router.php
@@ -12,40 +12,22 @@ if (in_array( $_SERVER["REQUEST_URI"], [ '/books.html', '/', '' ] )) {
     die;
 }
 if(preg_match('/\.(?:css)$/', $_SERVER["REQUEST_URI"])) {
-    if ($path["css"] == "css") {
-        header("Content-Type: text/css");
-        readfile($_SERVER["SCRIPT_FILENAME"]);
-    }
-    else {
-        return FALSE;
-    }
+    header("Content-Type: text/css");
+    readfile($_SERVER["SCRIPT_FILENAME"]);
+    
 }
 if(preg_match('/\.(?:js)$/', $_SERVER["REQUEST_URI"])) {
-    if ($path["js"] == "js") {
-        header("Content-Type: text/js");
-        readfile($_SERVER["SCRIPT_FILENAME"]);
-    }
-    else {
-        return FALSE;
-    }
+    header("Content-Type: text/js");
+    readfile($_SERVER["SCRIPT_FILENAME"]);
 }
 if(preg_match('/\.(?:ttf)$/', $_SERVER["REQUEST_URI"])) {
-    if ($path["ttf"] == "ttf") {
-        header("Content-Type: text/ttf");
-        readfile($_SERVER["SCRIPT_FILENAME"]);
-    }
-    else {
-        return FALSE;
-    }
+    header("Content-Type: text/ttf");
+    readfile($_SERVER["SCRIPT_FILENAME"]);
+
 }
 if(preg_match('/\.(?:woff2)$/', $_SERVER["REQUEST_URI"])) {
-    if ($path["woff2"] == "woff2") {
-        header("Content-Type: text/woff2");
-        readfile($_SERVER["SCRIPT_FILENAME"]);
-    }
-    else {
-        return FALSE;
-    }
+    header("Content-Type: text/woff2");
+    readfile($_SERVER["SCRIPT_FILENAME"]);
 }
 
 if (preg_match('/\/(img+)\/([^\/]+)/', $_SERVER["REQUEST_URI"], $matches)){
